4fb84cfa59d7e51a4fa212657464b209a26597a6
bulafish <38581839+bulafish@users.noreply.github.com>
2019-06-02 16:11:55 +0800

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -54,6 +54,4 @@ systemctl restart docker
 
 kubeadm init
 
-export KUBECONFIG=/etc/kubernetes/admin.conf
-
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
